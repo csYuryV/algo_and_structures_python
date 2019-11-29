b6317e5217194f0d7c8ed67bb6f222a3fa2c3489
cs <cs.yury.v@pm.me>
2019-11-29 19:44:18 +0300

    Сделал 1-2 задачки к уроку 5.

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -1,7 +1,7 @@
-"""
+'''
 
 2019-11-27 Sikorskiy Yuriy
-
+cs.yury.v@pm.me
 
 1. Пользователь вводит данные о количестве предприятий, их наименования и
 прибыль за 4 квартала (т.е. 4 отдельных числа) для каждого предприятия..
@@ -9,6 +9,133 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 
+'''
+
+from collections import namedtuple
+
+Quarters = namedtuple(
+    'Quarters',
+    'first_quarter second_quarter third_quarter fourth_quarter')
+quarters = Quarters(
+    'первый квартал',
+    'второй квартал',
+    'третий квартал',
+    'четвертый квартал')
 
 
-"""
+class Firm:
+    def __init__(self, name):
+        self._name = name
+        self._profit = dict()
+        self._total_profit = 0
+        self._average_profit_for_year = 0
+
+    def get_name(self):
+        return self._name
+
+    def get_profit(self):
+        return self._profit
+
+    def get_profit(self, key):
+        return self._profit[key]
+
+    def add_profit(self, key, value):
+        self._profit[key] = value
+
+    def get_average_profit_for_year(self):
+        return self._average_profit_for_year
+
+    def calc_average_profit_for_year(self):
+        self._total_profit = sum(self._profit.values())
+        self._average_profit_for_year = self._total_profit / 4
+        return self._average_profit_for_year
+
+
+class Firms:
+    def __init__(self):
+        self._list = list()
+        self._total_profit = 0
+        self._average_profit_for_year = 0
+
+    def get_list(self):
+        return self._list
+
+    def add_firm(self, firm):
+        self._list.append(firm)
+
+    def calc_average_profit_for_year(self):
+        self._average_profit_for_year = 0
+        for firm in self._list:
+            self._total_profit += firm.calc_average_profit_for_year()
+        self._average_profit_for_year = self._total_profit / len(self._list)
+        return self._average_profit_for_year
+
+    def find_average_profit_lower(self):
+        return [firm for firm in self._list if firm.get_average_profit_for_year(
+        ) < self._average_profit_for_year]
+
+    def find_average_profit_higher(self):
+        return [firm for firm in self._list if firm.get_average_profit_for_year(
+        ) > self._average_profit_for_year]
+
+
+def input_n():
+    MSG_ValueError = 'Необходимо ввести положительное целое число.'
+    while True:
+        try:
+            n = int(input('Введите количество фирм: '))
+            if n <= 0:
+                print(MSG_ValueError)
+                continue
+            break
+        except ValueError:
+            print(MSG_ValueError)
+            continue
+    return n
+
+
+def input_name_firm(i):
+    return input(f'Введите имя фирмы номер {i + 1}: ')
+
+
+def input_profit(j):
+    MSG_ValueError = 'Необходимо ввести действительное число.'
+    while True:
+        try:
+            n = float(input(f'Прибыль за {j}: '))
+            break
+        except ValueError:
+            print(MSG_ValueError)
+            continue
+    return n
+
+
+def main():
+    n = input_n()
+    firms = Firms()
+    for i in range(n):
+        firm = Firm(input_name_firm(i))
+
+        for j in quarters:
+            firm.add_profit(j, input_profit(j))
+            pass
+
+        firms.add_firm(firm)
+    print(
+        f'Средняя приыбыль за год для всех фирм составила: {firms.calc_average_profit_for_year():.2f}.')
+    high_margin_firms = firms.find_average_profit_higher()
+    low_margin_firms = firms.find_average_profit_lower()
+    if len(low_margin_firms) == 0:
+        print(f'Уникальная ситуция: среднегодовые прибыли всех фирм равны.')
+    else:
+        print(f'Список фирм, чья прибыль выше среднего:')
+        for firm in high_margin_firms:
+            print(f'{firm.get_name()} -> {firm.get_average_profit_for_year():.2f}.')
+
+        print(f'Список фирм, чья прибыль ниже среднего:')
+        for firm in low_margin_firms:
+            print(f'{firm.get_name()} -> {firm.get_average_profit_for_year():.2f}.')
+
+
+if __name__ == '__main__':
+    main()
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -1,7 +1,181 @@
-"""
+'''
+
+
+
 2. Написать программу сложения и умножения двух шестнадцатеричных чисел.
 При этом каждое число представляется как массив, элементы которого это цифры
 числа. Например, пользователь ввёл A2 и C4F. Сохранить их как [‘A’, ‘2’] и
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
-"""
+
+'''
+from collections import deque
+
+dict_hex = {
+    '0': 0x0,
+    '1': 0x1,
+    '2': 0x2,
+    '3': 0x3,
+    '4': 0x4,
+    '5': 0x5,
+    '6': 0x6,
+    '7': 0x7,
+    '8': 0x8,
+    '9': 0x9,
+    'A': 0xa,
+    'B': 0xb,
+    'C': 0xc,
+    'D': 0xd,
+    'E': 0xe,
+    'F': 0xf}
+
+list_hex = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F']
+set_hex = set(list_hex)
+
+
+# 16 ричные числа представлены в виде реверсированных колекций deque в которых более младшему разряду числа
+# соответствует меньший индекс элемента колекции
+
+def add_one_bit_hex(a, b):
+    # к этой функции можно применитьтехнологию мемоизации с ипользованием двухмерного словаря
+    # сложение одноразрядных 16-ричных числа
+    total = dict_hex[a] + dict_hex[b]
+    return deque([list_hex[total % 16], list_hex[total // 16]])
+
+
+def add_zeros(a, number_of_zeros=1, side=0):
+    # a колекция типа deque, side - указывает с какой стороны добавлять нули,
+    # 0 - слева, 1 - справа
+    zeros = ['0' for _ in range(number_of_zeros)]
+    if side == 0:
+        a.extendleft(zeros)
+    if side == 1:
+        a.extend(zeros)
+
+
+def nonzero_high_bit(list_two_bit_numbers):
+    for i in list_two_bit_numbers:
+        if i[1] != '0':
+            return True
+    return False
+
+
+def next_recount(list_two_bit_numbers):
+    res = []
+    res.append(deque([list_two_bit_numbers[0][0], list_hex[0]]))
+
+    for i in range(1, len(list_two_bit_numbers)):
+        res.append(add_one_bit_hex(
+            list_two_bit_numbers[i - 1][1], list_two_bit_numbers[i][0]))
+    if dict_hex[res[-1][1]] > 0:
+        res.append(deque(['0', '0']))
+    return res
+
+
+def add_multi_bit_hex(a, b):
+    # a, b - колекции типа deque
+
+    delta_len = abs(len(a) - len(b))
+    if delta_len > 0:
+        if len(a) < len(b):
+            add_zeros(a, delta_len, side=1)
+        else:
+            add_zeros(b, delta_len, side=1)
+    ab = list(zip(a, b))
+    res_bit_addition = []  # колекция промеуточных результатов побитового сложения разрядов
+    for couple in ab:
+        res_bit_addition.append(add_one_bit_hex(couple[0], couple[1]))
+    res_bit_addition.append(deque(['0', '0']))
+    # Проаеряю старшие биты если есть ненулевые сновы повторяю процедуру
+    # побитового сложения со смещением
+
+    if len(res_bit_addition) > 1:
+        while nonzero_high_bit(res_bit_addition):
+            res_bit_addition = next_recount(res_bit_addition)
+
+    if len(res_bit_addition) == 1:
+        result_add = deque(res_bit_addition)
+    else:
+        result_add = deque([i[0] for i in res_bit_addition])
+        while result_add[-1] == '0' and len(result_add) > 1:
+            result_add.pop()
+        return result_add
+
+
+def multiply_one_bit_hex(a, b):
+    # к этой функции можно применитьтехнологию мемоизации с ипользованием двухмерного словаря
+    # умножение одноразрядных 16-ричных числа
+    total = dict_hex[a] * dict_hex[b]
+    return deque([list_hex[total % 16], list_hex[total // 16]])
+
+
+def multiply_multi_bit_hex(a, b):
+    res = []
+    for i in range(len(a)):
+        for j in range(len(b)):
+            number_of_zeros = i + j
+            res_multiply_one_bit_hex = multiply_one_bit_hex(a[i], b[j])
+            if number_of_zeros > 0:
+                add_zeros(res_multiply_one_bit_hex, number_of_zeros)
+            res.append(res_multiply_one_bit_hex)
+    if len(res) == 1:
+        if res[0][1] == '0':
+            res[0].pop()
+        return res[0]
+    if len(res) >= 2:
+        res_multiply = add_multi_bit_hex(res[0], res[1])
+    if len(res) > 2:
+        for i in range(2, len(res)):
+            res_multiply = add_multi_bit_hex(res_multiply, res[i])
+    return res_multiply
+
+
+def input_hex(invitation):
+    MSG_ValueError = 'Некорректный ввод.'
+    correct_input = False
+    while not correct_input:
+        h = input(invitation).upper()
+        h_list = deque(list(h))
+        correct_input = True
+        for i in h_list:
+            if i not in set_hex:
+                print(MSG_ValueError)
+                correct_input = False
+                break
+    h_list.reverse()
+    return h_list
+
+
+def main():
+    print('Введите два шестнадцатиричных числа.')
+    a = input_hex('Введите первое шестнадцатиричное число: ')
+    b = input_hex('Введите второе шестнадцатиричное число: ')
+
+    res = add_multi_bit_hex(a, b)
+    res1 = multiply_multi_bit_hex(a, b)
+    a.reverse()
+    b.reverse()
+    res.reverse()
+    res1.reverse()
+    print(f'{a} + {b} = {res}')
+    print(f'{a} * {b} = {res1}')
+
+
+if __name__ == '__main__':
+    main()
